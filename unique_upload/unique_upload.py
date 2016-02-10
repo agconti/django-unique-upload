e2698d562e0d33d108b5447b757708184acc61dd
agconti <agc11d@gmail.com>
2016-02-10 13:51:14 -0500

    fix(unique_upload): updated import path for convenience

--- unique_upload/unique_upload.py
+++ unique_upload/unique_upload.py
@@ -3,5 +3,4 @@ import os
 
 def unique_upload(instance, filename):
     ext = filename.split('.').pop()
-    filename = "{}.{}".format(uuid.uuid4(), ext)
-    return os.path.join('uploads/logos', filename)
+    return "{}.{}".format(uuid.uuid4(), ext)
